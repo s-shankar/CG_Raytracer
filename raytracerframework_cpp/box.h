3d0d771727ac568a225b623e696b38d522966609
Shankar Sivagna <s.shankar@hotmail.fr>
2017-12-03 23:58:48 +0100

    pb pour trouver les bonnes coord

--- raytracerframework_cpp/box.h
+++ raytracerframework_cpp/box.h
@@ -1,12 +1,36 @@
 #ifndef BOX_H
 #define BOX_H
 
+#include <iostream>
+#include <vector>
 #include "object.h"
 
 class Box : public Object
 {
 public:
-	Box(Point v, double s) : vertex(v), edge(s) {}
+	Box(Point v, double s) : position(v), edge(s)
+	{
+		// get 4 points for the front face
+
+		Point p1 = position; // top left corner
+		Point p2 = position + (Point(edge, 0, 0)); // top right 
+		Point p3 = p2 - (Point(0, edge, 0)); // bottom right
+		Point p4 = p3 - (Point(edge, 0, 0)); // bottom left
+
+		Point p5 = p4 - (Point(0, 0, edge)); // bottom left behind
+		Point p6 = p5 + (Point(edge, 0, 0)); // bottom right behind
+		Point p7 = p6 + (Point(0, edge, 0)); // top right behind
+		Point p8 = p7 - (Point(edge, 0, 0)); // top left behind
+
+		vertex.push_back(p1);
+		vertex.push_back(p2);
+		vertex.push_back(p3);
+		vertex.push_back(p4);
+		vertex.push_back(p5);
+		vertex.push_back(p6);
+		vertex.push_back(p7);
+		vertex.push_back(p8);
+	}
 
 	virtual Hit intersect(const Ray &ray);
 	/*
@@ -16,7 +40,8 @@ public:
 	/* first vertex, then we can determine where the 7 others are
 		should we also consider direction of the box ?
 	*/
-	const Point vertex;
+	const Point position;
+	std::vector<Point> vertex;
 	const double edge;
 };
 
