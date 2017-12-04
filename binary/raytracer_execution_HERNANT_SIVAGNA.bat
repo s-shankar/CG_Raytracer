c7fda2657a3ccf1ad3f6c9c4ae5d2621f5215b00
Shankar Sivagna <s.shankar@hotmail.fr>
2017-12-04 05:01:05 +0100

    change .bat

--- binary/raytracer_execution_HERNANT_SIVAGNA.bat
+++ binary/raytracer_execution_HERNANT_SIVAGNA.bat
@@ -2,7 +2,6 @@
 echo Assignment 02 by Tristan Hernant and Shankar SIVAGNA
 start /d %~dp0 RayTracer.exe scene01-zbuffer.yaml
 start /d %~dp0 RayTracer.exe scene01-normal.yaml
-start /d %~dp0 RayTracer.exe scene01-normal-2nd_angle.yaml
 start /d %~dp0 RayTracer.exe customscene-zbuffer.yaml
 echo Done
 pause
